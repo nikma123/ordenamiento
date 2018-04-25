3dfe50aa6ea39a02d8998eadccd863fd56ce4f6b
nicolas <nicolas@DESKTOP-5Q69GIS>
2018-04-24 20:10:03 -0500

    se creo el ordenamiento burbuja

--- src/estudiante/Ordenamiento.java
+++ src/estudiante/Ordenamiento.java
@@ -5,10 +5,6 @@
  */
 package estudiante;
 
-/**
- *
- * @author nicolas
- */
 public class Ordenamiento {
     public static void main(String[] args) {
         Estudiante[]arrayEstudiante = new Estudiante[5];
@@ -18,6 +14,25 @@ public class Ordenamiento {
         arrayEstudiante[3] = new Estudiante("juan","lopez","216666",2,4.2);
         arrayEstudiante[4] = new Estudiante("lupe","andrade","10456",1,2.0);
         arrayEstudiante[5] = new Estudiante("felix","loé","1082287",6,3.5);
+        
+        int temp;
+        
+        for(int i=1;i < arrayEstudiante.length;i++){
+            for (int j=0 ; j < arrayEstudiante.length- 1; j++){
+                if (arrayEstudiante[j].getNota() < arrayEstudiante[j+1].getNota()){
+                    temp = arrayEstudiante[j].getPuesto();
+                    arrayEstudiante[j].setPuesto(arrayEstudiante[j+1].getPuesto());
+                    arrayEstudiante[j+1].setPuesto(temp);
+                }
+            }
+        }
+            for(int i=0;i < arrayEstudiante.length;i++){
+                
+                
+                System.out.println(arrayEstudiante[i]);
+                    
+                
+            }
     }
     
 }
